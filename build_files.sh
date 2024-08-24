95899e58cc86c774ee7b3d1ec53a2a88ee24cd27
Bobby <bobbyskhs@gmail.com>
2024-08-24 19:11:18 -0400

    even more scaffolding

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,15 @@
+echo "Building files..."
+mkdir -p static
+
 # Install Python packages
+echo "Installing Python packages..."
 pip3 install -r requirements.txt
 
+# Database migrations
+echo "Appling database migrations..."
+python3.12 manage.py makemigrations
+python3.12 manage.py migrate
+
 # Collect static files
+echo "Collecting static files..."
 python3.12 manage.py collectstatic --noinput
