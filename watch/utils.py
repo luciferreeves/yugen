283654eaed68de9225521185e79e29ebc1d79cca
Bobby <30593201+luciferreeves@users.noreply.github.com>
2025-01-19 17:59:27 -0500

    Update utils.py

--- watch/utils.py
+++ watch/utils.py
@@ -18,11 +18,11 @@ from watch.tmdbmapper import get_anime_episodes as gae, get_tv_episode_group_det
 
 dotenv.load_dotenv()
 
-r = redis.Redis(
-    host=os.getenv("REDIS_HOST"),
-    port=os.getenv("REDIS_PORT"),
-    password=os.getenv("REDIS_PASSWORD"),
-)
+# r = redis.Redis(
+#     host=os.getenv("REDIS_HOST"),
+#     port=os.getenv("REDIS_PORT"),
+#     password=os.getenv("REDIS_PASSWORD"),
+# )
 
 r.flushall()
 print("Redis cache flushed")
